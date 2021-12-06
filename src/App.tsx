4a9695c672e07922c9a7366d483cd9bbe921c970
sampath <sampathvinayak1453@gmail.com>
2021-12-06 09:56:05 +0530

    add posts

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Counter from 'remote/counter';
 import Health from 'remote/health';
+import AddNewPost from 'remote/post';
 
 import "./index.css";
 
 const App = () => (
   <div className="container">
-  <Counter additionalCount={5}/>
-  <Health />
+    <Counter additionalCount={5} />
+    <Health />
+    <AddNewPost
+      onClick={(e) => {}}
+      onChange={(title, body) => {
+        console.log(title, body);
+      }}
+    />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
